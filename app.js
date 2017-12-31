b48a15795bd9cd0f183d81d4cff662aea5576808
alicht <avilichtschein@gmail.com>
2017-12-31 14:19:00 -0500

    Add ability to edit movies

--- app.js
+++ app.js
@@ -16,5 +16,13 @@ app.listen(PORT, () => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
 const moviesRouter = require('./routes/movies-routes');
 app.use('/movies', moviesRouter)
